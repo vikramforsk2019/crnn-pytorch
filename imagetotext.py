fa674ac06c23410b555bd8249a0f41f5ee4a173f
vikramforsk2019 <vsg16492cse2016@gmail.com>
2020-04-03 17:03:44 +0530

    read

---
+++ imagetotext.py
@@ -0,0 +1,49 @@
+import pytesseract
+from PIL import Image 
+import argparse
+import cv2
+import os
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image to be OCR'd")
+ap.add_argument("-p", "--preprocess", type=str, default="thresh",
+	help="type of preprocessing to be done")
+args = vars(ap.parse_args())
+
+# load the example image and convert it to grayscale
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# check to see if we should apply thresholding to preprocess the
+# image
+if args["preprocess"] == "thresh":
+	gray = cv2.threshold(gray, 0, 255,
+		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+# make a check to see if median blurring should be done to remove
+# noise
+elif args["preprocess"] == "blur":
+	gray = cv2.medianBlur(gray, 3)
+# write the grayscale image to disk as a temporary file so we can
+# apply OCR to it
+filename = "{}.png".format(os.getpid())
+cv2.imwrite(filename, gray)
+
+
+# load the image as a PIL/Pillow image, apply OCR, and then delete
+# the temporary file
+text = pytesseract.image_to_string(Image.open(filename))
+os.remove(filename)
+print('car plate number>>')
+print(text)
+# show the output images
+cv2.imshow("Image", image)
+cv2.imshow("Output", gray)
+cv2.waitKey(0)
+
+
+"""
+ Python-tesseract is an optical character recognition (OCR) tool for python. 
+ That is, it will recognize and “read” the text embedded in images.
+ Tesseract is finding templates in pixels, letters, words and sentences. It uses two-step approach that calls adaptive recognition. It requires one data stage for character recognition, 
+ then the second stage to fulfil any letters, it wasn't insured in, by letters that can match the word or sentence context.
+"""
